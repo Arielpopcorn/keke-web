1affcdd3978e65e236d51913429e47e35247ad74
Ariel Chen <arielclimbing@gmail.com>
2021-08-26 21:00:23 +0300

    added redirect

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {
   Switch,
   Route,
   Link,
+  Redirect
 } from "react-router-dom";
 import researchgate from './constants/images/researchgate.svg';
 import twitter from './constants/images/inaturalist.svg';
@@ -217,6 +218,10 @@ function App() {
       </Header>
       <BodyContainer>
         <Switch>
+          <Redirect
+            exact
+            from="/"
+            to={HomePage} />
           <Route path="/contact">
             <ContactPage />
           </Route>
