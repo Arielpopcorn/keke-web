ad2f06753e4a018439e280cc090d260672341857
Ariel Chen <arielclimbing@gmail.com>
2021-09-14 18:03:41 +0100

    put cover back from research list

--- src/constants/researchList.js
+++ src/constants/researchList.js
@@ -11,6 +11,7 @@ export const RESEARCH_LIST = [{
   id: "1",
   slides: [RunHorseLight_1, RunHorseLight_2, RunHorseLight_3],
   title: "Temporal dimension of PSF",
+  cover: img1,
   tags:  ['All', 'Plant-soil feedback', 'Ontogenetic niche shift'],
   paragraph: [
     "Plant conditioning of soil microbial communities is a time-dependent process, but how PSF strengths vary with the duration of soil conditioning and whether plants differ in their temporal development pattern of PSF remains unknown. This information is critical when predicting the consequences of PSF because plant individuals may experience different microbial effects depending on how long neighboring plants have conditioned the soil. In the past, we have tackled this problem with annual aerial photos of a Californian foredune ecosystem. Using GIS spatial analysis, we estimated the age of individual plants and reconstructed >20-year chronosequence of soil conditioning length for all four dominant plants in the community. With a combination of high-throughput sequencing and greenhouse experiments, we have shown that soil microbial communities are continuously turning over due to plant conditioning and the PSF resulting from these shifts in microbial communities exhibit different temporal patterns depending on the plant-soil combination (Ke et al., 2021, New Phytologist).",
@@ -21,6 +22,7 @@ export const RESEARCH_LIST = [{
     id: "2",
     slides: [img2],
     title: "Mechanistic understanding of PSF",
+    cover: img2,
     tags:  ['All', 'Plant-soil feedback'],
     paragraph: [
       "Focusing on plant-soil feedback, I have developed models to investigate the interdependency among two major belowground processes: plant-mediated nutrient cycling (i.e., litter-mediated PSF) and plant-soil microbe interactions (i.e., microbial-mediated PSF). I have shown that plant litter decomposability, a key trait controlling nutrient cycling, has a strong positive effect on a plant’s PSF strength when mycorrhizal fungi are abundant, but not when pathogens dominate the microbial community. This result stems from the fact that the two microbial functional groups have different indirect interactions with litter dynamics. In particular, mycorrhizal fungi create an environment with depleted soil nutrient and enhanced litter production, and therefore the benefits of having higher litter decomposability are amplified under such an environment. Data compiled from empirical studies also supported this theoretical prediction (Ke et al., 2015, New Phytol.; Ke & Miki, 2015, Front. Microbiol.). Inspired by these results, I aim to build novel mechanistic models that further integrate litter- and microbial-mediated PSFs into resource competition theory, thereby better predicting the consequences of PSF across environmental contexts.",
@@ -29,6 +31,7 @@ export const RESEARCH_LIST = [{
     id: "3",
     slides: [img3],
     title: "Linking PSF and coexistence theory",
+    cover: img3,
     tags: ['All', 'Plant-soil feedback', 'Species coexistence'],
     paragraph: [
       "I have also demonstrated that the phenomenological perspective of MCT provides a framework to contextualize the diverse effects soil microbes have on plant coexistence (Ke & Wan, 2020, Ecol. Monogr.). By applying MCT to a mechanistic model of plant-soil feedback, I clarified how the interaction network predicts whether soil microbes act primarily as stabilizing or equalizing forces, and how soil microbes affect the importance of plant-plant competition for resources. By applying our predictive framework to experimental data, I have shown that the microbial effects that determine plant coexistence might not be the ones with the largest magnitude but instead depends on the context of plant-plant competition.",
@@ -37,6 +40,7 @@ export const RESEARCH_LIST = [{
     id: "4",
     slides: [img4],
     title: "Synthesizing coexistence theories",
+    cover: img4,
     tags: ['All', 'Species coexistence'],
     paragraph: [
       "The maintenance of biodiversity boils down to the stable coexistence of interacting species. When developing models to describe competitive interactions, two approaches are broadly applicable to various organisms. Some ecologists adopt a phenomenological approach, which seeks to capture an aggregated measure of the negative effects species have on each other and does not require full knowledge on how species compete (i.e., Lotka-Volterra models). In contrast, others adopt a mechanistic approach, which attempts to gain a highly resolved understanding of the competitive process by directly modeling the limiting factors underlying competition (e.g., consumer-resource models). Despite shared goals, little communication and translation of governing principles across the two perspectives preclude further conceptual advances at their interface.",
@@ -46,6 +50,7 @@ export const RESEARCH_LIST = [{
     id: "5",
     slides: [img5],
     title: "Stage dependent species interactions",
+    cover: img5,   
     tags: ['All', 'Ontogenetic niche shift'],
     paragraph: [
       "More content coming soon.",
